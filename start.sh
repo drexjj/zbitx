79fefb36daf8ffb79a2aca7df2ab22411636ae45
jj <155218248+drexjj@users.noreply.github.com>
2026-09-03 15:41:27 -0500

    v5.05 start script update (#13)
    * cleanup for public
    
    * Update update
    
    * Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
 sudo fuser -vu /dev/snd/* /dev/snd/by-path/*
-cd sbitx
-./zbitx
+cd /home/pi/sbitx
+
+if [  -f  "terminal.log"  ]; then
+    mv terminal.log previous.log
+fi
+set -o pipefail
+stdbuf -oL -eL ./zbitx 2>&1  |  tee terminal.log
+
 bash
 read -p "Press enter to continue..."
-
