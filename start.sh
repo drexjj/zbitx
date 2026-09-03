00e9b83b27a5dd135af4d0bb53633657d1297a7e
jj <155218248+drexjj@users.noreply.github.com>
2026-09-03 16:51:08 -0500

    Remove LANG and LC_ALL exports from start.sh
    Removed environment variable exports for LANG and LC_ALL.

--- start.sh
+++ start.sh
@@ -2,9 +2,6 @@
 sudo fuser -vu /dev/snd/* /dev/snd/by-path/*
 cd /home/pi/sbitx
 
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-
 if [  -f  "terminal.log"  ]; then
     mv terminal.log previous.log
 fi
