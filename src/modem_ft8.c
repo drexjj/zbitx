e22f624e887a55ad50538f028100b6ed4ae9fed1
jj <155218248+drexjj@users.noreply.github.com>
2026-09-14 21:51:53 -0500

    Update modem_ft8.c

--- src/modem_ft8.c
+++ src/modem_ft8.c
@@ -806,7 +806,7 @@ void ft8_on_signal_report(){
 	if (m3[0] == 'R'){
 		//skip the 'R'
 		field_set("RECV", m3+1);
-		ft8_tx(call, mycall, "RR73");  	
+		sprintf(reply_message, "%s %s RR73", call, mycall);  	
 		ft8_tx(reply_message, tx_pitch);
 	}
 	else{ 
